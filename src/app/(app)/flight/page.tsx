f2ebb624fee2a1d4a44447a9fa2b1ca051b9eba8
sarvagyapatel <103870100+sarvagyapatel@users.noreply.github.com>
2024-07-31 14:11:32 +0530

    flight page added

--- src/app/(app)/flight/page.tsx
+++ src/app/(app)/flight/page.tsx
@@ -1,24 +1,41 @@
 'use client'
 
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 import axios, { AxiosError } from 'axios'
+import FlightCard from '@/components/FlightCard';
+import { Loader2 } from 'lucide-react';
 
-type flightData = {
-
-}
+type FlightDetails = {
+  airline: string,
+  departure: {
+    time: string,
+    city: string,
+  },
+  arrival: {
+    time: string,
+    city: string,
+  },
+  duration: string,
+  stops: string,
+  price: string,
+};
 
 function FlightSearch() {
+
+  const [departureFlights, setDepartureFlights] = useState<FlightDetails[]>([]);
+  const [returnFlights, setReturnFlights] = useState<FlightDetails[]>([]);
+  const [loading, setLoading] = useState(false)
 
   useEffect(() => {
     const localData = localStorage.getItem('searchData') as string;
     const getData = async () => {
       try {
+        setLoading(true)
         const flightSearchValues = JSON.parse(localData);
         const originCode = (flightSearchValues.flightSearch.originCode);
-        const destinationCode= (flightSearchValues.flightSearch.destinationCode);
-        const amadeusFlightUrl = `https://test.api.amadeus.com/v2/shopping/flight-offers?originLocationCode=${originCode}&destinationLocationCode=${destinationCode}&departureDate=${flightSearchValues.flightSearch.startDate}&returnDate=${flightSearchValues.flightSearch.endDate}&adults=1&max=50`;
+        const destinationCode = (flightSearchValues.flightSearch.destinationCode);
 
-        const flightData =await axios(amadeusFlightUrl,
+        const departureFlightData = await axios(`https://test.api.amadeus.com/v2/shopping/flight-offers?originLocationCode=${originCode}&destinationLocationCode=${destinationCode}&departureDate=${flightSearchValues.flightSearch.startDate}&adults=1&max=10`,
           {
             headers: {
               Authorization: `Bearer ${flightSearchValues.flightSearch.access_token}`,
@@ -26,19 +43,87 @@ function FlightSearch() {
           }
         )
 
-        console.log(flightData)
+        const returnFlightData = await axios(`https://test.api.amadeus.com/v2/shopping/flight-offers?originLocationCode=${originCode}&destinationLocationCode=${destinationCode}&departureDate=${flightSearchValues.flightSearch.endDate}&adults=1&max=10`,
+          {
+            headers: {
+              Authorization: `Bearer ${flightSearchValues.flightSearch.access_token}`,
+            }
+          }
+        )
+
+        const newFlights: FlightDetails[] = departureFlightData.data.data.map((flight: any) => (
+          {
+            airline: departureFlightData.data.dictionaries.carriers[flight.itineraries[0].segments[0].carrierCode],
+            departure: {
+              time: flight.itineraries[0].segments[0].departure.at.split('T')[1],
+              city: flight.itineraries[0].segments[0].departure.iataCode,
+            },
+            arrival: {
+              time: flight.itineraries[0].segments[0].arrival.at.split('T')[1],
+              city: flight.itineraries[0].segments[0].arrival.iataCode,
+            },
+            duration: (flight.itineraries[0].segments[0].duration).replace("PT", ""),
+            stops: (flight.itineraries[0].segments).length - 1,
+            price: `₹ ${Math.floor(flight.travelerPricings[0].price.total * 90)}`,
+          }
+        ))
+
+        const newFlights1: FlightDetails[] = returnFlightData.data.data.map((flight: any) => (
+          {
+            airline: returnFlightData.data.dictionaries.carriers[flight.itineraries[0].segments[0].carrierCode],
+            departure: {
+              time: flight.itineraries[0].segments[0].departure.at.split('T')[1],
+              city: flight.itineraries[0].segments[0].departure.iataCode,
+            },
+            arrival: {
+              time: flight.itineraries[0].segments[0].arrival.at.split('T')[1],
+              city: flight.itineraries[0].segments[0].arrival.iataCode,
+            },
+            duration: (flight.itineraries[0].segments[0].duration).replace("PT", ""),
+            stops: (flight.itineraries[0].segments).length - 1,
+            price: `₹ ${Math.floor(flight.travelerPricings[0].price.total * 90)}`,
+          }
+        ))
+        console.log(departureFlightData)
+        setDepartureFlights(newFlights);
+        setReturnFlights(newFlights1);
       } catch (error) {
         console.error('Error fetching data:', error);
+      } finally {
+        setLoading(false)
       }
     };
     getData();
   }, []);
 
-  return (
-    <div className="absolute top-0 z-[-2] h-max min-h-screen w-screen bg-neutral-950 bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))]" >
-
-    </div>
-  )
+  if (loading) {
+    return (
+      <div className='w-full h-screen flex items-center justify-center'>
+        <Loader2 className='text-gray-700 h-36 w-36 animate-spin' />
+      </div>
+    );
+  } else {
+    return (
+      <div className='flex flex-wrap items-center justify-center'>
+        <div className="min-h-screen flex flex-col gap-4 items-center justify-center px-2">
+          <h1 className='mt-8 text-2xl font-bold -mb-4 w-full flex items-center justify-center rounded-3xl border-4'>Departure</h1>
+          {departureFlights.map((flight, index) => (
+            <div key={index} className="flex flex-wrap gap-4 items-center justify-center" >
+              <FlightCard flight={flight} />
+            </div>
+          ))}
+        </div>
+        <div className="min-h-screen flex flex-col gap-4 items-center justify-center px-2">
+          <h1 className='mt-8 text-2xl font-bold -mb-4   w-full flex items-center justify-center rounded-3xl border-4'>Return</h1>
+          {returnFlights.map((flight, index) => (
+            <div key={index} className="flex flex-wrap gap-4 items-center justify-center" >
+              <FlightCard flight={flight} />
+            </div>
+          ))}
+        </div>
+      </div>
+    );
+  }
 }
 
 export default FlightSearch
---
+++ src/components/FlightCard.tsx
@@ -0,0 +1,40 @@
+'use client'
+
+import React from 'react';
+
+const FlightCard = ({ flight }) => {
+    return (
+        <div className="w-fit mx-auto bg-white shadow-md rounded-lg overflow-hidden my-4 border border-gray-200">
+            <div className="flex items-center p-4 bg-blue-50">
+                <h2 className="text-gray-800 text-lg font-semibold">{flight.airline}</h2>
+            </div>
+            <div className="p-4 flex flex-row items-center justify-center">
+                <div className="flex flex-wrap gap-14 items-center justify-center" >
+                    <div>
+                        <p className="text-xl font-bold text-gray-800">{flight.departure.time}</p>
+                        <p className="text-sm text-gray-500">{flight.departure.city}</p>
+                    </div>
+                    <div className="flex flex-col items-center justify-center">
+                        <p className="text-gray-600 text-xs">{flight.duration}</p>
+                        <div className="h-1 w-full bg-green-500 mt-1 mb-1"></div>
+                        <p className="text-green-500 text-xs">{flight.stops}</p>
+                    </div>
+                    <div>
+                        <p className="text-xl font-bold text-gray-800">{flight.arrival.time}</p>
+                        <p className="text-sm text-gray-500">{flight.arrival.city}</p>
+                    </div>
+                    <div className="flex flex-col justify-between items-center">
+                        <p className="text-lg font-semibold text-gray-800">{flight.price}</p>
+                        <p className="text-sm text-gray-500">per adult</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+
+
+export default FlightCard;
+
+
